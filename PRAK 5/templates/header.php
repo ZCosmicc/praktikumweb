38e34646064e3b0004403aa5edced5b55df54d01
ZCosmicc <101439750+ZCosmicc@users.noreply.github.com>
2022-05-16 00:51:18 +0800

    Add files via upload

---
+++ PRAK 5/templates/header.php
@@ -0,0 +1,29 @@
+<head>
+    <title>Online Library</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style  type="text/css">
+        .library{
+            background: #cbb09c !important;
+        }
+        .library-text{
+            color: #cbb09c !important;
+        }
+        form{
+            max-width: 540px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+    </style>
+</head>
+    <body class="grey lighten-4">
+        <nav class="white z-depth-0">
+            <div class="container">
+                <a href="index.php" class="library-logo library-text">Online Library</a>
+                <ul id="nav-mobile" class="right hide-on-small-down">
+                    <li><a  href="FormMember.php" class="btn library z-depth-0">Register</a></li>
+                    <li><a  href="FormPeminjaman.php" class="btn library z-depth-0">Borrow</a></li>
+                    <li><a  href="FormBuku.php" class="btn library z-depth-0">Add Book</a></li>
+                </ul>
+            </div>
+        </nav>
